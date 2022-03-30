d66af9a42ab353d777ab5590a60a0dec0025ce9e
Gamaliel Burgos <54601555+Gamygams1234@users.noreply.github.com>
2022-03-29 17:13:33 -0700

    making a scanner program

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,28 @@
+import java.util.Scanner;
 
 public class Main {
 	
 	public static void main(String[] args) {
-		Dog dog = new Dog();
-		dog.name = "John";
-		dog.bark();
-
-		dog.age= 1;
-		dog.breed = "Golden Retriever";
-	
-		System.out.println(dog.name);
+		String name;
+		int age;
+		float shoeSize;
 		
-		Dog otherDog = new Dog();
-		otherDog.name = "Fido";
-		otherDog.bark();
-
-		otherDog.age= 3;
-		otherDog.breed = "Lab";
+		
+		Scanner userInput = new Scanner(System.in);
+		System.out.println("Please enter your name.");
+		// this will wait for the user to input and hit enter
+		name = userInput.nextLine();
+		System.out.println(name);
+		System.out.println("Please enter your age.");
+		// this will wait for the user to input and hit enter
+		age = userInput.nextInt();
+		System.out.println(age);
+		System.out.println("Please enter your shoesize");
+		// this will wait for the user to input and hit enter
+		shoeSize = userInput.nextFloat();
+		System.out.println(shoeSize);
+		
+		System.out.println("Your name is "+ name +". Your age is "+age+". Your shoe size is "+shoeSize);
 	}
 
 }
