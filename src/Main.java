5f21a92bfd92735f9f80eec9911e393c7bb68593
Gamaliel Burgos <54601555+Gamygams1234@users.noreply.github.com>
2022-03-29 22:27:04 -0700

    Making some methods in java

--- src/Main.java
+++ src/Main.java
@@ -3,26 +3,22 @@ import java.util.Scanner;
 public class Main {
 	
 	public static void main(String[] args) {
-		String name;
-		int age;
-		float shoeSize;
-		
-		
-		Scanner userInput = new Scanner(System.in);
-		System.out.println("Please enter your name.");
-		// this will wait for the user to input and hit enter
-		name = userInput.nextLine();
-		System.out.println(name);
-		System.out.println("Please enter your age.");
-		// this will wait for the user to input and hit enter
-		age = userInput.nextInt();
-		System.out.println(age);
-		System.out.println("Please enter your shoesize");
-		// this will wait for the user to input and hit enter
-		shoeSize = userInput.nextFloat();
-		System.out.println(shoeSize);
-		
-		System.out.println("Your name is "+ name +". Your age is "+age+". Your shoe size is "+shoeSize);
+		int num1 = 8;
+		int num2 = 16;
+	
+		System.out.println("The sum is "+ add(num1, num2));
+		sayHi();
 	}
+	// private means that it will only be accessed by this class
+	
+	private static int add(int a, int b){
+		return a + b;
+		
+	}
+	
+	private static void sayHi() {
+		System.out.println("Hi");
+	}
+
 
 }
