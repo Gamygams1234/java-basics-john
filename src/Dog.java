8f8b8551098b0e16189f73dc8684d9ff87ec9342
Gamaliel Burgos <54601555+Gamygams1234@users.noreply.github.com>
2022-03-30 00:24:54 -0700

    making constructors

--- src/Dog.java
+++ src/Dog.java
@@ -7,6 +7,17 @@ public class Dog {
 	String color;
 	int numberOfLegs;
 	
+	// default constuctor no args
+	public Dog() {
+		this.name = "Lassie";
+		this.numberOfLegs=4;
+	}
+	
+	public Dog(String name, int age) {
+		this.name = name;
+		this.age = age;
+		this.numberOfLegs=4;
+	}
 	void bark() {
 		System.out.println("Bark, my name is " + this.name);
 	}
--- src/Main.java
+++ src/Main.java
@@ -4,32 +4,13 @@ import java.util.Scanner;
 public class Main {
 	
 	public static void main(String[] args) {
-		Random rand = new Random();
-		int randomNumber = rand.nextInt(100) + 1;
 		
-		int guess;
-		Scanner userInput = new Scanner(System.in);
-		System.out.println("Welcome to the guessing game.");
-		System.out.println("Please enter your guess.");
-		guess = userInput.nextInt();
-		
-
+		// defalut constructonr
+			Dog myDog = new Dog("Hulk", 12);
+			Dog yourDog = new Dog();
 			
-			while(guess!= randomNumber) {
-			if (guess< randomNumber) {
-				System.out.println("You have to guess higher. Try again");
-				guess = userInput.nextInt();
-				
-				}else {
-					System.out.println("You have to guess lower. Try again");
-					guess = userInput.nextInt();
-				}
-			}
-		
-			if (guess == randomNumber) {
-				System.out.println("Bitch you guessed it! You was right!");
-				System.out.println("The number was " + guess);
-			}	
+			System.out.println(myDog.name);
+			System.out.println(yourDog.name);
 
 	}
 }
