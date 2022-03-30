2cd9cb3c0da2e281824648cc616b7278da35668c
Gamaliel Burgos <54601555+Gamygams1234@users.noreply.github.com>
2022-03-30 00:44:33 -0700

    Working with the static word

--- src/Dog.java
+++ src/Dog.java
@@ -1,25 +1,26 @@
 
 
 public class Dog {
+	
+	static int dogCount = 0;
 	String name;
 	int age;
 	String breed;
 	String color;
 	int numberOfLegs;
 	
-	// default constuctor no args
 	public Dog() {
-		this.name = "Lassie";
-		this.numberOfLegs=4;
+		this.dogCount++;
 	}
 	
-	public Dog(String name, int age) {
-		this.name = name;
-		this.age = age;
-		this.numberOfLegs=4;
-	}
+
+
 	void bark() {
 		System.out.println("Bark, my name is " + this.name);
+	}
+	
+	static void printMetaDogInfo() {
+		System.out.println("Total dogs in program "+ dogCount);
 	}
 
 }
--- src/Main.java
+++ src/Main.java
@@ -6,11 +6,18 @@ public class Main {
 	public static void main(String[] args) {
 		
 		// defalut constructonr
-			Dog myDog = new Dog("Hulk", 12);
+	
+			Dog myDog = new Dog();
+			
 			Dog yourDog = new Dog();
 			
-			System.out.println(myDog.name);
-			System.out.println(yourDog.name);
+			
+			myDog.bark();
+			System.out.println(Dog.dogCount);
+			
+			Dog.printMetaDogInfo();
+			
+	
 
 	}
 }
